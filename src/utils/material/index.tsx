c53af3e6fb669e8c459f63e51ba90ca679bb0b44
渡辺拓馬 <takuma_0920@i.softbank.jp>
2020-11-19 15:40:40 +0900

    一旦コミット

---
+++ src/utils/material/index.tsx
@@ -0,0 +1,44 @@
+//ロゴ
+import React from "react"
+import { makeStyles } from "@material-ui/core/styles"
+import Card from "@material-ui/core/Card"
+import CardHeader from "@material-ui/core/CardHeader"
+import CardMedia from "@material-ui/core/CardMedia"
+import CardContent from "@material-ui/core/CardContent"
+import CardActions from "@material-ui/core/CardActions"
+import Collapse from "@material-ui/core/Collapse"
+import Avatar from "@material-ui/core/Avatar"
+import IconButton from "@material-ui/core/IconButton"
+import Typography from "@material-ui/core/Typography"
+import { red } from "@material-ui/core/colors"
+import FavoriteIcon from "@material-ui/icons/Favorite"
+import ShareIcon from "@material-ui/icons/Share"
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore"
+import MoreVertIcon from "@material-ui/icons/MoreVert"
+import CardActionArea from "@material-ui/core/CardActionArea"
+import Button from "@material-ui/core/Button"
+//フォーマット
+import MediaCard from "./MediaCard"
+
+export {
+  //ロゴ
+  Card,
+  CardHeader,
+  CardMedia,
+  CardContent,
+  CardActions,
+  Collapse,
+  Avatar,
+  IconButton,
+  Typography,
+  FavoriteIcon,
+  ShareIcon,
+  ExpandMoreIcon,
+  MoreVertIcon,
+  makeStyles,
+  red,
+  Button,
+  CardActionArea,
+  //フォーマット
+  MediaCard
+}
