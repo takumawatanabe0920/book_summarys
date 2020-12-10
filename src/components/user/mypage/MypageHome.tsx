d5e3f6b3c50a3f577c44efbdb28b6231062247b2
渡辺拓馬 <takuma_0920@i.softbank.jp>
2020-12-10 11:27:35 +0900

    ユーザー情報UI

---
+++ src/components/user/mypage/MypageHome.tsx
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from "react"
+import { useParams } from "react-router-dom"
+import { ResultResponse, ResUser } from "../../../types"
+import { MypageSidebar, MypageProfile } from "../.."
+import { getIdUser } from "../../../firebase/functions"
+
+const MypageEdit = () => {
+  const [user, setUser] = useState<ResUser>({})
+  const [loading, setLoading] = useState<boolean>(false)
+  const url: { id: string } = useParams()
+
+  useEffect(() => {
+    const loadData = async () => {
+      try {
+        const resUser: ResultResponse<ResUser> = await getIdUser(url.id)
+        console.log(resUser)
+        if (resUser && resUser.status === 200) {
+          setUser(resUser.data)
+        }
+      } catch (e) {}
+    }
+
+    loadData()
+    setLoading(true)
+  }, [])
+
+  return (
+    <>
+      {loading && Object.keys(user).length > 0 && (
+        <div className="mypage_main">
+          <div className="l-container">
+            <div className="main-block _block-center">
+              <div className="user-mypage">
+                <h1 className="main-title blue-main-title">MY PAGE</h1>
+                <div className="mypage-content">
+                  <MypageSidebar user={user} />
+                  <div className="_mypage">
+                    <h2 className="sub-ttl">会員情報</h2>
+                    <MypageProfile user={user} />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  )
+}
+
+export default MypageEdit
---
+++ src/components/user/mypage/parts/MypageProfile.tsx
@@ -0,0 +1,51 @@
+import React, { useState, useEffect, FC } from "react"
+import clsx from "clsx"
+import { ResUser } from "../../../../types"
+import { responseUploadImage } from "../../../../firebase/functions"
+import { userCircleIcon } from "../../../../utils/icons"
+
+type Props = {
+  user: ResUser
+}
+
+const MypageSidebar: FC<Props> = props => {
+  const [loading, setLoading] = useState<boolean>(false)
+  const [userIcon, setUserIcon] = useState<string>("")
+  const { user } = props
+
+  useEffect(() => {
+    const loadData = async () => {
+      try {
+        console.log(user)
+        const resUserIcon: string = await responseUploadImage(user.photoURL)
+        console.log(resUserIcon)
+        setUserIcon(resUserIcon)
+      } catch (e) {}
+    }
+
+    loadData()
+    setLoading(true)
+  }, [])
+
+  return (
+    <>
+      {loading && (
+        <div className="_user-profile">
+          <div className="_user-image">
+            <img src={userIcon ? userIcon : userCircleIcon} alt="ロゴ" />
+          </div>
+          <dl>
+            <dt className="_label">名前</dt>
+            <dd className="_user-name">{user.displayName}</dd>
+          </dl>
+          <dl>
+            <dt className="_label">メールアドレス</dt>
+            <dd className="_user-name">{user.email}</dd>
+          </dl>
+        </div>
+      )}
+    </>
+  )
+}
+
+export default MypageSidebar
